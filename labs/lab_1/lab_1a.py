61d3a83a1b49821c7e93aab53cdf1ae79133dfc5
NoobMaster9999 <omarorafas1@gmail.com>
2026-02-21 11:04:56 -0600

    Add introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Amrit Arora" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Amrit, and I am currently a freshmen in Austin, Texas. I love cybersecurity and taking part in Capture-The-Flag (CTFs). I enjoy playing basketball on my school's team. Looking forward to working with you all!")
 
 if __name__ == "__main__":
     main()
